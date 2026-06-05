d20c49824d25806f226219a8e0344fd57c799706
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2026-06-05 06:49:21 -0700

    Update the default light mode color to be easier on the eyes
    Update pages/minecraft/modpacks/cetw/index.vue

--- app/pages/minecraft/modpacks/cetw/index.vue
+++ app/pages/minecraft/modpacks/cetw/index.vue
@@ -1,6 +1,6 @@
 <template>
   <UContainer>
-    <UPageHeader
+    <UPageHero
       title="Create: Explore The World!"
       description="A semi-nomadic modpack that will make you explore but allows you to create a base anywhere you want! You will have to setup base eventually otherwise you won't have enough storage for all your loot and technology, also how are you supposed to create a movable power source? (wait you can do that...)"
     />
